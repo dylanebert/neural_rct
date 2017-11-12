2897c30887f0b74262218ced9c1a60f4ef78180c
Dylan <dylan.ebert@gmail.com>
2017-11-11 22:23:00 -0500

    ..

--- nn.py
+++ nn.py
@@ -1,43 +1,48 @@
-from read_td6 import *
 import tensorflow as tf
 import sys
+import os
 import numpy as np
+from segments import segment_dict
 
-batch_size = 5
+batch_size = 10
 window_size = 25
 embed_size = 100
-num_epochs = 100
 
 def indexed(data):
 	indices = dict()
-	i = 0
+	indices[0xff] = 0
+	i = 1
 	for segment in data:
 		if segment not in indices:
 			indices[segment] = i
 			i += 1
+	inverse_indices = dict(zip(indices.values(), indices.keys()))
 	indexed = [indices[segment] for segment in data if segment in indices]
-	return indexed, indices, len(indices)
+	vocab_size = len(indices)
+	return indexed, indices, inverse_indices, vocab_size
 	
 def batched(data, batch_size, window_size):
-	num_batches = len(data) // batch_size	
-	data = np.reshape(data[0 : batch_size * num_batches], [batch_size, num_batches])	
-	epoch_size = (num_batches - 1) // window_size	
+	num_batches = len(data) // batch_size
+	data = np.reshape(data[0 : batch_size * num_batches], [batch_size, num_batches])
+	epoch_size = (num_batches - 1) // window_size
 	x = list()
 	y = list()
 	for i in range(epoch_size):
 		x.append(data[:, i * window_size : (i + 1) * window_size])
 		y.append(data[:, i * window_size + 1 : (i + 1) * window_size + 1])
-	x = np.array(x)	
-	y = np.array(y)
+	x = np.array(x, dtype=np.uint8)
+	y = np.array(y, dtype=np.uint8)
 	return x, y
 
+if len(sys.argv) != 2:
+	sys.exit('Invalid syntax. Use nn [dirname]')
 dir = sys.argv[1]
-tracks_raw = get_raw_data(dir)
-tracks, indices, vocab_size = indexed(tracks_raw)
-inverse_indices = dict(zip(indices.values(), indices.keys()))
-
-tracks_train = tracks[:-100]
-tracks_test = tracks[-100:]
+files = os.listdir(dir)
+data = []
+for file in files:
+	with open('{0}/{1}'.format(dir, file), 'rb') as f:
+		data += f.read()
+data_indexed, indices, inverse_indices, vocab_size = indexed(data)
 
 inpt = tf.placeholder(tf.int32, shape = [batch_size, window_size])
 answr = tf.placeholder(tf.int32, shape = [batch_size, window_size])
@@ -57,16 +62,16 @@ logits = tf.reshape(logits_1, [batch_size, window_size, vocab_size])
 
 xEnt = tf.contrib.seq2seq.sequence_loss(logits, answr, tf.ones([batch_size, window_size], dtype = tf.float32), average_across_timesteps = False, average_across_batch = True)
 loss = tf.reduce_sum(xEnt)
-train = tf.train.AdamOptimizer(.001).minimize(loss)
+train = tf.train.AdamOptimizer(1e-3).minimize(loss)
 
 sess = tf.Session()
 init = tf.global_variables_initializer()
 sess.run(init)
 	
-for _ in range(num_epochs):
+x, y = batched(data_indexed, batch_size, window_size)	
+for epoch in range(num_epochs):
 	avg_loss = 0
 	loss_count = 0
-	x, y = batched(tracks_train, batch_size, window_size)
 	for i in range(len(x)):
 		inpts = x[i]
 		answrs = y[i]
@@ -74,12 +79,12 @@ for _ in range(num_epochs):
 		avg_loss += loss_val
 		loss_count += 1
 	avg_loss /= float(loss_count)
-	print(avg_loss)
+	print('{0}: {1}'.format(epoch, avg_loss))
 		
-print('Begin testing')
+'''print('Begin testing')
 avg_loss = 0
 loss_count = 0
-x, y = batched(tracks_train, batch_size, window_size)
+x, y = batched(tracks_test, batch_size, window_size)
 for i in range(len(x)):
 	inpts = x[i]
 	answrs = y[i]
@@ -87,14 +92,17 @@ for i in range(len(x)):
 	avg_loss += loss_val
 	loss_count += 1
 avg_loss /= float(loss_count)
-print(avg_loss)
+print(avg_loss)'''
 
 print('Begin building')
-state = x[0]
+state = np.zeros([batch_size, window_size])
 i = 0
 while i < 100:
 	inpts = state
 	res = sess.run(logits, feed_dict = {inpt: inpts})
 	state = [[np.argmax(res[j][k]) for k in range(len(state[j]))] for j in range(len(state))]
-	print(segment_dict[inverse_indices[state[0][-1]]])
+	try:
+		print(segment_dict[inverse_indices[state[0][-1]]])
+	except:
+		print('{0:x}'.format(inverse_indices[state[0][-1]]))
 	i += 1
--- track_extractor.py
+++ track_extractor.py
@@ -2,8 +2,6 @@ import sys
 import os
 import numpy as np
 
-track_size = 250
-
 class TrackError(Exception):
 	def __init__(self, value):
 		self.value = value
@@ -21,18 +19,17 @@ def clean_file(filename, dirname):
 	while i > 2 and data[i-2] == 3:
 		i -= 2
 	start_i = i
-	bytes = np.ones(track_size, dtype=np.uint8) * 0xff
-	j = 0
 	segment_counters = dict()
 	qualifiers = list()
+	bytes = list()
 	odd_start = 0
 	consec_odd = 0
-	while i < len(data) - 3 and data[i] != 0xff and j < track_size:
+	while i < len(data) - 3 and data[i] != 0xff:
 		#if data[i] > 0xda:
 			#raise TrackError('Invalid track piece {0:x}: {1:x} on {2}'.format(i, data[i], filename))
 		if data[i-1] == 0xff:
 			raise TrackError('Detected shift at {0:x} on {1}. Started at {2:x}'.format(i, filename, start_i))
-		bytes[j] = data[i]
+		bytes.append(data[i])
 		if data[i] not in segment_counters:
 			segment_counters[data[i]] = 0
 		segment_counters[data[i]] += 1
@@ -42,22 +39,24 @@ def clean_file(filename, dirname):
 				odd_start = i
 			elif consec_odd > 2:
 				while i > start_i:
-					bytes[j] = 0xff
+					bytes.pop()
 					segment_counters[data[i]] -= 1
 					if segment_counters[data[i]] == 0:
 						i += 1
 						consec_odd = 0
 						break
-					j -= 1
 					i -= 2					
 				#raise TrackError('Error detected starting near {0:x} on {1}'.format(i, filename))
 			consec_odd += 1
 		else:
 			consec_odd = 0
 		i += 2
-		j += 1
 	if data[i-2] != 2 and data[i-2] != 3:
 		raise TrackError('Track \'{0}\' does not end with station. Start {1:x}: {2:x}; End {3:x}: {4:x}'.format(filename, start_i, data[start_i], i-2, data[i-2]))
+	if not os.path.exists('extracted_tracks'):
+		os.makedirs('extracted_tracks')
+	while len(bytes) % 25 != 0:
+		bytes.append(0xff)
 	with open('extracted_tracks/{1}'.format(dirname, filename), 'wb') as f:
 		f.write(bytearray(bytes))
 	os.remove(path)
