89a956c9357c497d8161fd8d5dc03ad6e3000d7e
Dylan <dylan.ebert@gmail.com>
2017-11-11 19:43:37 -0500

    ..

--- filter.py
+++ filter.py
@@ -4,16 +4,21 @@ import numpy as np
 
 def filter(tracktype, dirname):
 	filtered = list()
+	new_dir = '{0}/filtered'.format(dirname)
+	if not os.path.exists(new_dir):
+		os.makedirs(new_dir)
 	for filename in os.listdir(dirname):
+		if os.path.isdir('{0}/{1}'.format(dirname, filename)):
+			continue
 		data = np.fromfile('{0}/{1}'.format(dirname, filename), dtype=np.uint8)
-		if data[1] != int(tracktype, 16):
+		if data[1] == int(tracktype, 16):
 			filtered.append(filename)
 	for filename in filtered:
 		print(filename)
-		os.remove('{0}/{1}'.format(dirname, filename))
-	
+		with open('{0}/{1}'.format(new_dir, filename), 'wb') as f:
+			f.write(bytearray(np.fromfile('{0}/{1}'.format(dirname, filename), dtype=np.uint8)))
 
 if __name__ == '__main__':
 	if len(sys.argv) != 3:
-		sys.exit('Invalid syntax. Use filter [tracktype] [dirname]')
-	filter(sys.argv[1], sys.argv[2])
+		sys.exit('Invalid syntax. Use filter [dirname] [tracktype]')
+	filter(sys.argv[2], sys.argv[1])
--- track_extractor.py
+++ track_extractor.py
@@ -10,40 +10,76 @@ class TrackError(Exception):
 	def __str__(self):
 		return repr(self.value)
 
-def clean_file(filename):
-	data = np.fromfile(filename, dtype=np.uint8)
+def clean_file(filename, dirname):
+	path = '{0}/{1}'.format(dirname, filename)
+	if os.path.isdir(path):
+		return
+	data = np.fromfile(path, dtype=np.uint8)
 	i = 0xa3
 	while i > 0 and data[i] != 1:
 		i -= 1
 	while i > 2 and data[i-2] == 3:
 		i -= 2
-	bytes = np.zeros(track_size, dtype=np.uint8)
+	start_i = i
+	bytes = np.ones(track_size, dtype=np.uint8) * 0xff
 	j = 0
+	segment_counters = dict()
+	qualifiers = list()
+	odd_start = 0
+	consec_odd = 0
 	while i < len(data) - 3 and data[i] != 0xff and j < track_size:
+		#if data[i] > 0xda:
+			#raise TrackError('Invalid track piece {0:x}: {1:x} on {2}'.format(i, data[i], filename))
+		if data[i-1] == 0xff:
+			raise TrackError('Detected shift at {0:x} on {1}. Started at {2:x}'.format(i, filename, start_i))
 		bytes[j] = data[i]
+		if data[i] not in segment_counters:
+			segment_counters[data[i]] = 0
+		segment_counters[data[i]] += 1
+		if data[i+1] not in qualifiers:
+			qualifiers.append(data[i+1])
+			if consec_odd == 0:
+				odd_start = i
+			elif consec_odd > 2:
+				while i > start_i:
+					bytes[j] = 0xff
+					segment_counters[data[i]] -= 1
+					if segment_counters[data[i]] == 0:
+						i += 1
+						consec_odd = 0
+						break
+					j -= 1
+					i -= 2					
+				#raise TrackError('Error detected starting near {0:x} on {1}'.format(i, filename))
+			consec_odd += 1
+		else:
+			consec_odd = 0
 		i += 2
 		j += 1
 	if data[i-2] != 2 and data[i-2] != 3:
-		raise TrackError('Track \'{0}\' does not end with station. Ends with {1:x}'.format(filename, data[i-2]))
-	for byte in bytes:
-		pass
-		#print('{0:x}'.format(byte))
+		raise TrackError('Track \'{0}\' does not end with station. Start {1:x}: {2:x}; End {3:x}: {4:x}'.format(filename, start_i, data[start_i], i-2, data[i-2]))
+	with open('extracted_tracks/{1}'.format(dirname, filename), 'wb') as f:
+		f.write(bytearray(bytes))
+	os.remove(path)
 
 def clean_dir(dirname):
 	for filename in os.listdir(dirname):
 		try:
-			clean_file('{0}/{1}'.format(dirname, filename))
+			clean_file(filename, dirname)
 		except TrackError as e:
 			print(e)
+	if len(os.listdir(dirname)) == 0:
+		os.rmdir(dirname)
+		return
 		
 if __name__ == '__main__':
 	if len(sys.argv) < 2:
 		sys.exit('Invalid syntax. Use \'track_extractor [filename]\' or \'track_extractor -r [dirname]\'')	
 	if len(sys.argv) == 3 and sys.argv[1] == '-r':
 		clean_dir(sys.argv[2])
-	elif len(sys.argv) == 2:
+	elif len(sys.argv) == 3:
 		try:
-			clean_file(sys.argv[1])
+			clean_file(sys.argv[2], sys.argv[1])
 		except TrackError as e:
 			sys.exit(e)
 	else:
